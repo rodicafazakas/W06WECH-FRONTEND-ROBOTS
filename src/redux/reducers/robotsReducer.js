60ffa2f633dce974a29fe1a68c7b92212cbf289f
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-11-06 14:05:38 +0100

    Defined robotsReducer

---
+++ src/redux/reducers/robotsReducer.js
@@ -0,0 +1,29 @@
+import actionTypes from "../actions/actionTypes";
+
+const robotsReducer = (robots=[], action) => {
+let newRobots;
+
+switch (action.type) {
+  case (actionTypes.loadRobots):
+    newRobots = [...action.robots];
+    break;
+  case (actionTypes.createRobot):
+    newRobots = [...robots, action.robot];
+    break;
+  case (actionTypes.updateRobot):
+    newRobots = [
+      ...robots.filter( robot => robot.id !== action.id),
+      action.robot,
+    ];
+    break;
+  case (actionTypes.deleteRobot):
+    newRobots = robots.filter( robot => robot.id !== action.id);
+    break;
+  default:
+    newRobots = robots;
+};
+
+return newRobots;
+};
+
+export default robotsReducer;
---
+++ src/redux/reducers/robotsReducer.test.js
@@ -0,0 +1,38 @@
+import robotsReducer from "./robotsReducer";
+import actionTypes from "../actions/actionTypes";
+
+describe("Given a robotsReducer", () => {
+  describe("When it receives an empty list of robots and a load action with a number of robots", () => {
+    test("The it should return a new list with the robots received", ()=> {})
+    const initialList = [];
+    const robotsList = [
+          {
+            id: 1,
+            name: "R2-D2",
+            image: "https://media.istockphoto.com/photos/-picture-id500962582?s=612x612",
+            features: {
+              speed: 4,
+              strength: 9,
+              creationDate: 1977,
+            }
+          },
+          { id: 2,
+            name: "rodney",
+            image: "https://static.wikia.nocookie.net/robotcity/images/7/78/Rodney_Copperbottom.jpg/revision/latest?cb=20120220191544",
+            features: {
+              speed: 10,
+              strength: 9,
+              creationDate: 2015,
+            }
+          }
+        ];
+    const action = {
+      type: actionTypes.loadRobot,
+      robots: robotsList,
+    }
+
+    const newList = robotsReducer(initialList, action);
+
+    expect(newList).toEqual(robotsList);
+  })
+})
